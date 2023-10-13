df58992a7bacf199fb09d5a0fb98a6c2b17f0b6f
jaybz <jaybz.e@gmail.com>
2023-10-14 01:05:54 +0800

    Fix mobile display control sizes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const s2 = require('s2-cell-draw');
 const appName = 'MHNTerrainTool';
 const localStorageVersion = 1;
-const appVersion = '0.7.2';
+const appVersion = '0.7.3';
 const colorOrder = ['#ff9900', '#009933', '#cc00ff'];
 var knownCells = {};
 var polyList = [];
